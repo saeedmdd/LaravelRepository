5ddb48cf3f0af9568a7ca9f695fe1d0a1386495b
saeedmdd <saeed.madadi7@gmail.com>
2022-10-04 18:58:31 +0330

    dockrized and made migrations

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->slug,
+            "description" => $this->faker->text,
+            "user_id" => $this->faker->numberBetween(1,10)
+        ];
+    }
+}
